860aed66206ef00dbbb4242d24c0530080fdc79a
alfaindomart <alfaindomart23@gmail.com>
2025-07-29 16:57:41 +0700

    add truncate func

--- entrypoints/content/components/Menu.tsx
+++ entrypoints/content/components/Menu.tsx
@@ -1,14 +1,51 @@
 import { useState } from "react";
 import { ChevronRight } from "lucide-react";
+import background from "@/entrypoints/background";
 
 function OpenMenu() {
     const [isOpen, setIsOpen] = useState(false)
 
+
     return (
-        <button onClick={() => setIsOpen(!isOpen)}>
-            {isOpen ? <ChevronRight className="rotate-90" /> : <ChevronRight />}
-        </button>
+        <>
+            <button onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? <ChevronRight className="rotate-90" /> : <ChevronRight />}
+            </button>
+            <div className="h-height flex-col">
+                {isOpen && (
+    
+                    <div style={{backgroundColor: 'var(--bg-elevated-secondary)'}} className="w-64">
+                        <SidebarContent />
+                    </div>
+                )}
+            </div>
+        </>
     )
+}
+
+    function SidebarContent() {
+                const userQueries = [...document.querySelectorAll("[data-message-author-role='user']")] as HTMLElement[]
+                console.log(userQueries[0].dataset.messageId)
+                const testTrunc = userQueries.map((query) => truncate(query.innerText))
+
+                console.log(testTrunc)
+                
+                
+                return (
+                    <>
+                        {userQueries ? userQueries.forEach(query => {
+                            <div className="flex auto" data-id={query.dataset.messageId}>
+                                <div className="border-solid">{query.innerHTML}</div>
+                            </div>
+                        }) : <p>Can't find any message</p>}
+
+                    </>
+                )
+    }
+
+function truncate(queryContent: string) {
+    const truncated = queryContent.slice(0, 50)
+    return truncated
 }
 
 export default OpenMenu
