96e420c53ea200791f95e959a5be47e913715833
alfaindomart <alfaindomart23@gmail.com>
2025-09-23 21:10:01 +0700

    add popup entrypoint for managing all bookmarks

--- entrypoints/content/hooks/useBookmark.tsx
+++ entrypoints/content/hooks/useBookmark.tsx
@@ -35,37 +35,6 @@ export function useBookmark(currSite: Config) {
 
     }
 
-    // async function saveChat(query: HTMLElement) {
-
-    //         const newSave: Bookmark = {
-    //             key: currSite?.selectors.helper(query),
-    //             chatUrl: window.location.href,
-    //             previewChat: truncate(query.innerText),
-    //             timeStamp: Date.now()
-    //         }
-
-    //         const copyOfBookmark = new Map(bookmarked)  
-
-    //         if (copyOfBookmark.has(newSave.key)) {
-    //             console.log('the chat already exist, removing...')
-    //             copyOfBookmark.delete(newSave.key)
-    //         } else {
-    //             console.log('setting new key')
-    //             copyOfBookmark.set(newSave.key, newSave)
-    //         }
-
-    //         setBookmarked(copyOfBookmark)
-
-    //         console.log(bookmarked)
-
-    //         const updatedChats = Array.from(copyOfBookmark.values())
-    //         console.log('updating array')
-    //         console.log(updatedChats)
-    //         await storage.setItem("sync:SavedChats", updatedChats)
-    //     }
-
-    
-
      getSaveChats()
       }, [])
 
---
+++ entrypoints/popup/components/BookmarkManager.tsx
@@ -0,0 +1,36 @@
+import {useState, useEffect} from "react";
+import { Bookmark } from "@/entrypoints/content/config";
+import { Trash2 } from "lucide-react";
+
+export const BookmarkManager = () => {
+    const [bookmarks, setBookmarks] = useState([] as Array<Bookmark>);
+
+    useEffect(() => {
+        try {
+                const fetchBookmarks = async () => {
+                console.log('fetching bookmarks from storage...');
+                const SavedChats = await storage.getItem<Array<Bookmark>>('sync:SavedChats') || [] //'sync:SavedChats' !== 'sync: SavedChats', is space sensitive!
+                setBookmarks(SavedChats);
+               }
+               fetchBookmarks()
+            } catch(err) {
+                console.log('no bookmarks found', err);
+            }
+            
+        }, [])
+
+        return (
+        <div>
+            {bookmarks.map((bookmark) => (
+                <div>
+                    <div key={bookmark.key} className="p-2 m-2 border border-gray-600 rounded-lg">
+                        <p className="text-sm text-gray-300">Saved on: {bookmark.timeStamp.toLocaleString()}</p>
+                        <p className="text-md text-white">{bookmark.previewChat}</p>
+                        <a href={bookmark.chatUrl} target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:underline">Go to chat</a>
+                    </div>
+                    <button><Trash2/></button>
+                </div>
+            ))}
+        </div>
+        )
+}
